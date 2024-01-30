ef0069d75c64c43211340e03b83c13bb75b7a5d6
Gareth Bult <gareth@madpenguin.uk>
2024-01-30 12:12:43 +0000

    Sync

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@ apt install -y python3-pip git
 pip install pgpy jinja2 tqdm --break-system-packages
 mount -o remount,rw /run/live/findiso
 cd /run/live/findiso
-git update
+sudo git config --global --add safe.directory /run/live/findiso/universal_boot
+sudo git update
