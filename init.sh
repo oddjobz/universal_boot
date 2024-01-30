557625fad095ac94131382a74f59601cbaf7e0af
Gareth Bult <gareth@madpenguin.uk>
2024-01-30 10:13:55 +0000

    Add -y to apt get

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-apt install python3-pip git
+apt install -y python3-pip git
 pip install pgpy jinja2 tqdm --break-system-packages
 mount -o remount,rw /run/live/findiso
 cd /run/live/findiso
