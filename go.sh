d2f7823f1783fe20a2cd23fab0af01a91cbf8968
Gareth Bult <gareth@madpenguin.uk>
2024-01-30 12:08:13 +0000

    Add 'go'

---
+++ go.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+curl https://raw.githubusercontent.com/oddjobz/universal_boot/main/init.sh | sudo bash
--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ apt install -y python3-pip git
 pip install pgpy jinja2 tqdm --break-system-packages
 mount -o remount,rw /run/live/findiso
 cd /run/live/findiso
-https://github.com/oddjobz/universal_boot.git
+git update
