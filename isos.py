660a90ed68e394c321f6158a3e229ea24e4445a7
Gareth Bult <gareth@madpenguin.uk>
2025-08-06 18:04:35 +0000

    Sync

--- isos.py
+++ isos.py
@@ -391,10 +391,15 @@ iso_images = {
         'iso': 'https://mirror.bytemark.co.uk/linuxmint/stable/22.1/linuxmint-22.1-xfce-64bit.iso',
         'sum': '6451496af35e6855ffe1454f061993ea9cb884d2b4bc8bf17e7d5925ae2ae86d',
     },
-
-
-
-
+    'vanillaos-2025-02-20': {
+        'name': 'vanilla',
+        'menu': 'debian',
+        'cls': 'vanillaos',
+        'title': 'VanillaOS (2025-02)',
+        'size': 1.92,
+        'iso': 'https://github.com/Vanilla-OS/live-iso/releases/download/2.0/VanillaOS-2-stable.20250220.iso',
+        'sum': '87922b952d7b8c00e7acd48beb945f6c35e4415ce4cf89c4e7ac3a3df66c988f'
+    },
     'arch-2025.08.01': {
         'name': 'arch',
         'menu': 'arch',
@@ -475,15 +480,6 @@ iso_images = {
     #     'sum': 'aebdcb6b1859b21982021f69a1ca1e8b65e7628bc5a41650061dbbb7da6f4874',
     #     'sign': 'https://sourceforge.net/projects/mx-linux/files/Final/Fluxbox/MX-23.6.1_fluxbox_x64.iso.sig'
     # },
-    'vanillaos-2025-02-20': {
-        'name': 'vanilla',
-        'menu': 'debian',
-        'cls': 'vanillaos',
-        'title': 'VanillaOS (2025-02)',
-        'size': 1.92,
-        'iso': 'https://github.com/Vanilla-OS/live-iso/releases/download/2.0/VanillaOS-2-stable.20250220.iso',
-        'sum': '87922b952d7b8c00e7acd48beb945f6c35e4415ce4cf89c4e7ac3a3df66c988f'
-    },
 
 
 
